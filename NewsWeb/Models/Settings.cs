7680c2f3fb5861ddf95c5acbd7bf7b5403e004cd
Le <dle50@dxc.com>
2020-07-21 10:51:14 +0700

    initialize git

---
+++ NewsWeb/Models/Settings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace NewsWeb.Models
+{
+    public class Settings
+    {
+        #region Site Settings
+        public string HomeUrl { get; set; }
+        public string SiteLogoUrl { get; set; }
+        public IPublishedContent RootSiteContentNode { get; set; }
+        #endregion
+
+        #region Social
+        public string FacebookUrl { get; set; }
+        public string TwitterUrl { get; set; }
+        public string GoogleUrl { get; set; }
+        public string PinterestUrl { get; set; }
+        public string VimeoUrl { get; set; }
+        public string LinkedinUrl { get; set; }
+        #endregion
+
+        #region Meta Tags
+        public string DescriptionSuffix { get; set; }
+
+        #endregion
+    }
+}
