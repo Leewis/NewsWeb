c457a4458c274ecef28e3bf5aa10cfb4bf360f41
Nguyen <knguyen220@dxc.com>
2020-07-23 11:11:29 +0700

    Add project files.

---
+++ NewsWeb/Services/SettingsService.cs
@@ -0,0 +1,26 @@
+﻿using NewsWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Web;
+
+namespace NewsWeb.Services
+{
+    public class SettingsService
+    {
+        Settings siteSettings;
+
+        public Settings SiteSettings
+        {
+            get
+            {
+                if (siteSettings == null)
+                {
+                   
+                }
+                return siteSettings;
+            }
+        }
+    }
+}
