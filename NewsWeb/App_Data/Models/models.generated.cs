695ab0ada66b6caa15d9ccd637756f8583452f6b
Duc Huynh Le <lehades.duc@gmail.com>
2020-08-21 11:09:45 +0700

    replace CategoryFeautreNews by using one macro

--- NewsWeb/App_Data/Models/models.generated.cs
+++ NewsWeb/App_Data/Models/models.generated.cs
@@ -18,7 +18,7 @@ using Umbraco.Web;
 using Umbraco.ModelsBuilder.Embedded;
 
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "e33e4862a01f65c4")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 namespace Umbraco.Web.PublishedModels
 {
