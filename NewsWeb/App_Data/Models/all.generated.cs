bc14a04f8678c035a33cf910cf0bcb3f808eebb5
Khang Tran Minh Nguyen <knguyen220@dxc.com>
2020-09-01 16:32:35 +0700

    update missing code News.cshtml

--- NewsWeb/App_Data/Models/all.generated.cs
+++ NewsWeb/App_Data/Models/all.generated.cs
@@ -7,7 +7,7 @@ using  Umbraco.Core.Models.PublishedContent;
 using  Umbraco.Web;
 using  Umbraco.ModelsBuilder.Embedded;
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "f5fea5971e26baf4")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
